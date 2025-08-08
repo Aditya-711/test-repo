f20e16115ce3073b33c8a70db47845c2ce377e86
Aditya Inamdar <adityainamdar711@gmail.com>
2025-08-08 19:00:32 +0530

    added environments

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
         <title>Sample App</title>
       </head>
       <body>
-        <h1>Hello Aditya!</h1>
+        <h1>Hello Prashant!</h1>
         <p>This is a very very simple HTML page .</p>
       </body>
     </html>
